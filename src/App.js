0209863548cb16df9cd0d6eba53e6b153d3b9210
Jia <jiavon@ualberta.ca>
2020-07-30 13:44:50 -0600

    yoda branch created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
+import Yoda from './components/Yoda';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Services />
       <Contact />
+      <Yoda />
     </div>
   );
 }
---
+++ src/components/Yoda.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Yoda extends React.Component
+{
+    render()
+    {
+        return (
+            <>
+            <h1>
+            A programmer I am, by Yoda.
+            </h1>
+            <ul>
+                <li>To eat kale I like.</li>
+                <li>You please to meet, the greatest tiny green person ever I am.</li>
+                <li>The best star wars series star wars episode 1 is.</li>
+            </ul>
+            </>
+        );
+    }
+}
+
+export default Yoda;
