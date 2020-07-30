16dcf57cd539198a699ff16f3d1d7916e22179d7
Jia <jiavon@ualberta.ca>
2020-07-30 10:26:04 -0600

    added about component. to about branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <h1>Git Branching / Merge Practice</h1>
       <p>Let's practice Git Branching and Merging</p>
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class About extends React.Component
+{
+    render ()
+    {
+        return (
+            <p>
+                Brances help us work with teams; 
+                &nbsp;
+                maintaining the intergrity of our
+                &nbsp;
+                commit history!
+            </p>
+        );
+    }
+}
+export default About;
