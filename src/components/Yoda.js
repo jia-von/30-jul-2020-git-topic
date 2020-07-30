344958783e1c3a04b2d4ca3a4e1b3a96b8b578cd
Jia <jiavon@ualberta.ca>
2020-07-30 13:52:45 -0600

    additional commit

--- src/components/Yoda.js
+++ src/components/Yoda.js
@@ -13,6 +13,8 @@ class Yoda extends React.Component
                 <li>To eat kale I like.</li>
                 <li>You please to meet, the greatest tiny green person ever I am.</li>
                 <li>The best star wars series star wars episode 1 is.</li>
+                <li>he best star wars series star wars episode 1 i</li>
+                <li> he best star wars series star wars episode 1 i</li>
             </ul>
             </>
         );
